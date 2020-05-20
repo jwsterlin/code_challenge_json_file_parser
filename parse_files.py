ac64092543abc1fccbb1113d8d61e58950a23afc
Justin <jwsterlin@gmail.com>
2020-05-20 16:31:09 -0400

    Refactored a bit to clean up main

--- parse_files.py
+++ parse_files.py
@@ -15,6 +15,26 @@ DEFAULT_NUM_THREADS = 4
 DEFAULT_LOG_LEVEL = "INFO"
 
 def main():
+    args = get_args()
+    setup_logs(args.log_level.upper(), args.log_location)
+
+    logging.info("Running file parser")
+
+    with concurrent.futures.ProcessPoolExecutor() as executor:
+        json_files = glob.glob(args.input_dir + "/*")
+        for json_file, work_csv_file in zip(json_files, executor.map(parse_file, json_files)):
+            logging.debug(f"Temporary results for {json_file} stored at {work_csv_file}")
+
+def parse_file(file_location):
+    logging.debug(f"Parsing file: {file_location}")
+    return "somefile"
+
+def setup_logs(log_level, log_location):
+    log_format = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+    numeric_level = getattr(logging, log_level, None)
+    logging.basicConfig(filename=log_location, level=numeric_level, format=log_format)
+
+def get_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("-i", "--input_dir", required=True, help="Location of a directory that contains JSON files to be parsed.")
     parser.add_argument("-o", "--output_dir", required=True, help="Output directory location where CSV results will be stored.")
@@ -46,21 +66,7 @@ def main():
         default=DEFAULT_WORK_DIR,
         help=f"Directory where temporary CSV results will be stored.  Defaults to <current_directory>/{DEFAULT_WORK_DIR}."
     )
-    args = parser.parse_args()
-
-    log_format = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
-    numeric_level = getattr(logging, args.log_level.upper(), None)
-    logging.basicConfig(filename=args.log_location, level=numeric_level, format=log_format)
-    logging.info("Running file parser")
-
-    with concurrent.futures.ProcessPoolExecutor() as executor:
-        json_files = glob.glob(args.input_dir + "/*")
-        for json_file, work_csv_file in zip(json_files, executor.map(parse_file, json_files)):
-            logging.debug(f"Temporary results for {json_file} stored at {work_csv_file}")
-
-def parse_file(file_location):
-    logging.debug(f"Parsing file: {file_location}")
-    return "somefile"
+    return parser.parse_args()
 
 if __name__ == "__main__":
     main()

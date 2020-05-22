43a661998425b940100fb933b4c205c4cee0d395
Justin <jwsterlin@gmail.com>
2020-05-22 14:23:54 -0400

    Decided it's proper to leave that newline at the end, and reducing code complexity is a nice side effect

--- file_parser/file_parser.py
+++ file_parser/file_parser.py
@@ -11,27 +11,13 @@ class FileParser:
     def parse_file(self, input_file_location, output_file_location):
         logging.debug(f"File {input_file_location} will be parsed and output to {output_file_location}")
 
-        output = io.StringIO()
-        writer = csv.writer(output, delimiter=',', quoting=csv.QUOTE_MINIMAL)
-        is_first_line = True
-        line_was_valid = True
         with open(input_file_location, "r", newline='') as input_file:
             with open(output_file_location, "w", newline='') as output_file:
+                writer = csv.writer(output_file, delimiter=',', quoting=csv.QUOTE_MINIMAL)
                 for line in input_file:
-                    if not is_first_line:
-                        if line_was_valid:
-                            output_file.write(output.getvalue().rstrip() + "\n")
-                        output.truncate(0)
-                        output.seek(0)
-                    is_first_line = False
-
                     results = self.parse_line(line)
-                    if results == None:
-                        line_was_valid = False
-                    else:
-                        line_was_valid = True
+                    if results != None:
                         writer.writerow(results)
-                output_file.write(output.getvalue().rstrip())
 
     def parse_line(self, line):
         try:
@@ -60,12 +46,7 @@ class FileParser:
 
 
     def combine_csvs(self, input_csv_file_locs, output_csv_file_loc):
-        num_files = len(input_csv_file_locs)
-        cur_file = 0
         with open(output_csv_file_loc, "w") as output_csv_file:
             for input_csv_file_loc in input_csv_file_locs:
-                cur_file = cur_file + 1
                 with open(input_csv_file_loc, "r") as input_csv_file:
                     shutil.copyfileobj(input_csv_file, output_csv_file)
-                    if cur_file != num_files:
-                        output_csv_file.write("\n")
